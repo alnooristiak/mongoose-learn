94f4e027aaf957c9eae341280cb0cb933b2ab903
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-05-13 23:06:27 +0600

    creating moduler structure and user interface file

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,17 @@
+export interface IUser {
+    id: string;
+    role: string;
+    password: string;
+    name: {
+        firstName: string;
+        middleName?: string;
+        lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emargencyContactNo: string;
+    presentAddress: string;
+    parmanentAddress: string;
+};

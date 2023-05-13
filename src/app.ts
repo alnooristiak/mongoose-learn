bc7ba80b3d394210aaacbc3ec91083788409f1d6
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-05-13 21:42:47 +0600

    creating schema to interface

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { Schema } from 'mongoose';
 
 const app: Application = express()
 
@@ -8,7 +9,7 @@ app.use(cors());
 
 // parse data
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     // inserting a test data in to mongodb 
@@ -18,28 +19,85 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
      step3: model
      step4: database Query
      */
-    interface Iuser {
+    interface IUser {
         id: string;
         role: string;
         password: string;
         name: {
             firstName: string;
-            middleName: string;
+            middleName?: string;
             lastName: string;
         };
-        dateOfBirth ?: string;
-        gender: "male" | "female" ;
-        email ?: string;
+        dateOfBirth?: string;
+        gender: "male" | "female";
+        email?: string;
         contactNo: string;
         emargencyContactNo: string;
         presentAddress: string;
         parmanentAddress: string;
     }
+    // creating schema using interface IUser
+    const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            require: true,
+            unike: true,
+        },
+        role: {
+            type: String,
+            require: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        name: {
+            firstName: {
+                type: String,
+                require: true,
+            },
+            middleName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+                require: true,
+            },
+        },
+        dateOfBirth: {
+            type: String,
+        },
+        gender: { 
+            type: String,
+            enum: ["male", "female"]
+        },
+        email: {
+            type: String,
+        },
+        contactNo: {
+            type: String,
+            require: true,
+        },
+        emargencyContactNo: {
+            type: String,
+            require: true,
+        },
+        presentAddress: {
+            type: String,
+            require: true,
+        },
+        parmanentAddress: {
+            type: String,
+            require: true,
+        },
+        
 
-
-
+        // avatar: String
+    });
     // res.send('Hello World!');
     // next();
 })
+
+
 
 export default app;
